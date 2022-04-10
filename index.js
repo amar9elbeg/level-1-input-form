f144c82d76738c8db1eb3df264a4cb3cd7173b06
Chingun <zolboo@Chinguns-iMac.local>
2022-04-10 01:24:33 -0700

    finished the html and css but my html and css might be wrong

---
+++ index.js
@@ -0,0 +1,28 @@
+const firstName = document.getElementById('infoOne')
+const lastName = document.getElementById('infoTwo')
+const emailAdress = document.getElementById('infoThree')
+const websiteName = document.getElementById('infoFour')
+const writeYourMessage = document.getElementById('userMessage')
+
+firstName.addEventListener('click', function () {
+    
+    
+    firstName.style.color = '#5BA0E0'
+    
+})
+lastName.addEventListener('click', function () {
+    lastName.style.color = '#5BA0E0'
+    
+})
+emailAdress.addEventListener('click', function () {
+    emailAdress.style.color = '#5BA0E0'
+    
+})
+websiteName.addEventListener('click', function () {
+    websiteName.style.color = '#5BA0E0'
+    
+})
+writeYourMessage.addEventListener('click', function () {
+    writeYourMessage.style.color = '#5BA0E0'
+    
+})
