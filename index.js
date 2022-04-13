ad6c836a0175bc99c5e3e78012e7245a494342a1
Chingun <zolboo@Chinguns-iMac.local>
2022-04-13 00:32:48 -0700

    completed input form level 1

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-// const firstName = document.getElementById("firstName");
-// const firstNameError = document.getElementById("firstNameError");
-// if (firstName.value === "") {
-//   firstNameError.classList.remove("hide");
-// } else {
-//   firstNameError.classList.add("add");
-// }
+const firstName = document.getElementById("firstName");
+const firstNameError = document.getElementById("firstNameError");
 
-// const lastName = document.getElementById("infoTwo");
-// const emailAdress = document.getElementById("infoThree");
-// const websiteName = document.getElementById("infoFour");
-// const writeYourMessage = document.getElementById("userMessage");
+const emailAdress = document.getElementById("emailAdress");
+const emailError = document.getElementById("emailError");
 
-// firstName.addEventListener("click", function () {
-//   firstName.style.color = "#5BA0E0";
-// });
-// lastName.addEventListener("click", function () {
-//   lastName.style.color = "#5BA0E0";
-// });
-// emailAdress.addEventListener("click", function () {
-//   emailAdress.style.color = "#5BA0E0";
-// });
-// websiteName.addEventListener("click", function () {
-//   websiteName.style.color = "#5BA0E0";
-// });
-// writeYourMessage.addEventListener("click", function () {
-//   writeYourMessage.style.color = "#5BA0E0";
-// });
+const lastName = document.getElementById("lastName");
+const lastNameError = document.getElementById("lastNameError");
+
+const websiteName = document.getElementById("websiteName");
+const websiteError = document.getElementById("websiteError");
+
+const messageArea = document.getElementById("messageArea");
+const messageError = document.getElementById("messageError");
+
+const requiredFields = document.getElementsByClassName("requiredFields");
+
+submitButton.addEventListener("click", function () {
+  if (firstName.value === "") {
+    firstNameError.style.display = "flex";
+  } else console.log(firstName.value);
+});
+submitButton.addEventListener("click", function () {
+  if (emailAdress.value === "") {
+    emailError.style.display = "flex";
+  } else console.log(emailAdress.value);
+});
+submitButton.addEventListener("click", function () {
+  if (lastName.value === "") {
+    lastNameError.style.display = "flex";
+  } else console.log(lastName.value);
+});
+submitButton.addEventListener("click", function () {
+  if (websiteName.value === "") {
+    websiteError.style.display = "flex";
+  } else console.log(websiteName.value);
+});
+submitButton.addEventListener("click", function () {
+  if (messageArea.value === "") {
+    messageError.style.display = "flex";
+  } else console.log(messageArea.value);
+});
