ea357f18adbb4bbad93750593de58169fe9ea661
Chingun <zolboo@Chinguns-iMac.local>
2022-04-20 00:45:09 -0700

    completed button activation script

--- index.js
+++ index.js
@@ -38,11 +38,13 @@ const requiredFields = document.getElementsByClassName("requiredFields");
 
 const submitButton = document.getElementById("submitButton");
 
-let firstNameError = true;
-let lastNameError = true;
-let emailError = true;
-let firstNameError = true;
-let firstNameError = true;
+let firstNameErrors = true;
+let lastNameErrors = true;
+let emailErrors = true;
+let websiteErrors = true;
+let passwordErrors = true;
+let passwordErrorsConfirm = true;
+let messageErrors = true;
 
 function checkFirstName(value) {
   if (value === "") {
@@ -58,7 +60,8 @@ function checkFirstName(value) {
     firstNameErrorTwo.style.display = "none";
     firstNameError.style.display = "none";
     submitButton.removeAttribute("disabled");
-    firstNameError = false;
+    firstNameErrors = false;
+    console.log(value)
   }
 }
 
@@ -66,42 +69,54 @@ function checkLastName(value) {
   if (value === "") {
     lastNameError.style.display = "flex";
     lastNameErrorTwo.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
   } else if (value.length <= 3) {
     lastNameErrorTwo.style.display = "flex";
     lastNameError.style.display = "none";
     console.log("Insufficient Character Amount");
+    submitButton.setAttribute("disabled", "true");
   } else {
     lastNameErrorTwo.style.display = "none";
     lastNameError.style.display = "none";
     console.log(value);
+    submitButton.removeAttribute("disabled");
+    lastNameErrors = false;
   }
 }
 function checkEmail(value) {
   if (value === "") {
     emailError.style.display = "flex";
     emailErrorTwo.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
   } else if (value.length <= 3) {
     emailErrorTwo.style.display = "flex";
     emailError.style.display = "none";
     console.log("Insufficient Character Amount");
+    submitButton.setAttribute("disabled", "true");
   } else {
     emailErrorTwo.style.display = "none";
     emailError.style.display = "none";
     console.log(value);
+    submitButton.removeAttribute("disabled");
+    emailErrors = false;
   }
 }
 function checkWebsite(value) {
   if (value === "") {
     websiteError.style.display = "flex";
     websiteErrorTwo.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
   } else if (value.length <= 3) {
     websiteErrorTwo.style.display = "flex";
     websiteError.style.display = "none";
     console.log("Insufficient Character Amount");
+    submitButton.setAttribute("disabled", "true");
   } else {
     websiteErrorTwo.style.display = "none";
     websiteError.style.display = "none";
     console.log(value);
+    submitButton.removeAttribute("disabled");
+    websiteErrors = false;
   }
 }
 function checkPassword(valueOfPassword, valueOfConfirmPassword) {
@@ -110,35 +125,48 @@ function checkPassword(valueOfPassword, valueOfConfirmPassword) {
     passwordErrorTwo.style.display = "none";
     passwordErrorConfirm.style.display = "none";
     passwordErrorConfirmTwo.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
   } else if (valueOfPassword.length <= 7) {
     passwordErrorTwo.style.display = "flex";
     passwordError.style.display = "none";
     passwordErrorConfirm.style.display = "none";
     passwordErrorConfirmTwo.style.display = "none";
     console.log("Insufficient Character Amount");
+    submitButton.setAttribute("disabled", "true");
   } else if (valueOfConfirmPassword === "") {
     passwordErrorConfirm.style.display = "flex";
     passwordError.style.display = "none";
     passwordErrorTwo.style.display = "none";
     passwordErrorConfirmTwo.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
+    passwordErrors = false;
+    passwordErrorsConfirm = false;
   } else if (valueOfConfirmPassword !== valueOfPassword) {
     passwordErrorConfirmTwo.style.display = "flex";
     passwordErrorConfirm.style.display = "none";
     passwordError.style.display = "none";
     passwordErrorTwo.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
   } else {
     passwordErrorConfirm.style.display = "none";
     passwordErrorConfirmTwo.style.display = "none";
-    console.log(value);
+    console.log(valueOfPassword);
+    console.log(valueOfConfirmPassword);
+    submitButton.removeAttribute("disabled");
+    passwordErrors = false;
+    passwordErrorsConfirm = false;
   }
 }
 function checkMessage(value) {
   if (messageArea.value === "") {
     messageError.style.display = "flex";
     submitButton.disabled = "true";
+    submitButton.setAttribute("disabled", "true");
   } else {
     messageError.style.display = "none";
     console.log(value);
+    submitButton.removeAttribute("disabled");
+    messageErrors = false;
   }
 }
 
@@ -170,8 +198,11 @@ submitButton.addEventListener("click", function () {
   checkWebsite(websiteName.value);
   checkEmail(emailAdress.value);
   checkMessage(messageArea.value);
-  checkPassword(passwordName.value);
-  if (!firstNameError && !lastNameError && !emailError && !messageError && !websiteError && !passwordError) {
+  checkPassword(passwordName.value, passwordNameConfirm.value);
+  console.log("clickedButton")
+  if (!firstNameErrors && !lastNameErrors && !emailErrors && !websiteErrors && !messageErrors && !passwordErrors && !passwordErrorsConfirm === true) {
     navigateToSignIn.setAttribute("href", "./sign-in.html");
+  } else {
+    console.log("Error")
   }
 });
