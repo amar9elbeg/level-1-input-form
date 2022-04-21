61c69b57f4eb75625496ade1418eb79d87a3cd3f
Tyler <tyler@class-computers-MacBook-Air.local>
2022-04-20 18:25:36 -0700

    finished red text even more

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const firstNameMoreThan3 = document.getElementById("firstNameMoreThan3");
 
 const submitButton = document.getElementById("button");
 const lastName = document.getElementById("inputTwo");
-const lastNameRequired = document.getElementById("lastNameEmpty")
+const lastNameRequired = document.getElementById("lastNameEmpty");
 const lastNameMoreThan3 = document.getElementById("lastNameMoreThan3");
 
-const Email = document.getElementById("Email")
-const emailRequired = documen.getElementById("emailEmpty")
-const emailMoreThan3 = document.getAnimations("emailMoreThan3")
+const email = document.getElementById("inputThree");
+const emailRequired = document.getElementById("emailEmpty");
+const emailMoreThan3 = document.getElementById("emailMoreThan3");
 
+const web = document.getElementById("inputFour");
+const webRequired = document.getElementById("webEmpty");
+const webMoreThan3 = document.getElementById("webMoreThan3");
 
-const web = document.getElementById("web")
-const webRequired = document.getElementById("webEmpty")
-const webMoreThan3 = document.getElementById("webMoreThan3")
-
-const message = document.getElementById("message")
-const messageRequired = document.getElementById("messageEmpty")
-const messageMoreThan3 = document.getElementById("messageMoreThan3")
+const message = document.getElementById("inputFive");
+const messageRequired = document.getElementById("messageEmpty");
+const messageMoreThan3 = document.getElementById("messageMoreThan3");
 
 function validateMessage(value) {
   if (value.length === 0) {
     messageRequired.style.display = "flex";
+    messageMoreThan3.style.display = "none";
   } else if (value.length <= 3) {
     messageRequired.style.display = "none";
     messageMoreThan3.style.display = "flex";
@@ -30,6 +30,7 @@ function validateMessage(value) {
     messageRequired.style.display = "none";
     messageMoreThan3.style.display = "none";
   }
+}
 
 function validateWeb(value) {
   if (value.length === 0) {
@@ -41,6 +42,7 @@ function validateWeb(value) {
     webRequired.style.display = "none";
     webMoreThan3.style.display = "none";
   }
+}
 function validateEmail(value) {
   if (value.length === 0) {
     emailRequired.style.display = "flex";
@@ -51,36 +53,111 @@ function validateEmail(value) {
     emailRequired.style.display = "none";
     emailMoreThan3.style.display = "none";
   }
+
+  function validateMessage(value) {
+    if (value.length === 0) {
+      messageRequired.style.display = "flex";
+      messageMoreThan3.style.display = "none";
+      submitButton.setAttribute("disabled", "true");
+    } else if (value.length <= 3) {
+      messageRequired.style.display = "none";
+      messageMoreThan3.style.display = "flex";
+      submitButton.setAttribute("disabled", "true");
+    } else {
+      messageRequired.style.display = "none";
+      messagelMoreThan3.style.display = "none";
+      submitButton.removeAttribute("disabled");
+    }
+  }
+}
+function validateWeb(value) {
+  if (value.length === 0) {
+    webRequired.style.display = "flex";
+    webMoreThan3.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
+  } else if (value.length <= 3) {
+    webRequired.style.display = "none";
+    webMoreThan3.style.display = "flex";
+    submitButton.setAttribute("disabled", "true");
+  } else {
+    webRequired.style.display = "none";
+    webMoreThan3.style.display = "none";
+    submitButton.removeAttribute("disabled");
+  }
+}
+
+function validateEmail(value) {
+  if (value.length === 0) {
+    emailRequired.style.display = "flex";
+    emailMoreThan3.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
+  } else if (value.length <= 3) {
+    emailRequired.style.display = "none";
+    emailMoreThan3.style.display = "flex";
+    submitButton.setAttribute("disabled", "true");
+  } else {
+    emailRequired.style.display = "none";
+    emailMoreThan3.style.display = "none";
+    submitButton.removeAttribute("disabled");
+  }
 }
 
 function validateLastName(value) {
-    if (value.length === 0) {
-      lastNameRequired.style.display = "flex";
-    } else if (value.length <= 3) {
-      lastNameRequired.style.display = "none";
-      lastNameMoreThan3.style.display = "flex";
-    } else {
-      lastNameRequired.style.display = "none";
-      lastNameMoreThan3.style.display = "none";
-    }
+  if (value.length === 0) {
+    lastNameRequired.style.display = "flex";
+    lastNameMoreThan3.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
+  } else if (value.length <= 3) {
+    lastNameRequired.style.display = "none";
+    lastNameMoreThan3.style.display = "flex";
+    submitButton.setAttribute("disabled", "true");
+  } else {
+    lastNameRequired.style.display = "none";
+    lastNameMoreThan3.style.display = "none";
+    submitButton.removeAttribute("disabled");
   }
+}
 
 function validateFirstName(value) {
   if (value.length === 0) {
     firstNameRequired.style.display = "flex";
+    firstNameMoreThan3.style.display = "none";
+    submitButton.setAttribute("disabled", "true");
   } else if (value.length <= 3) {
     firstNameRequired.style.display = "none";
     firstNameMoreThan3.style.display = "flex";
+    submitButton.setAttribute("disabled", "true");
   } else {
     firstNameRequired.style.display = "none";
     firstNameMoreThan3.style.display = "none";
+    submitButton.removeAttribute("disabled");
   }
 }
 
-submitButton.addEventListener("click"), function () {
+firstName.onkeyup = function () {
   validateFirstName(firstName.value);
+};
+lastName.onkeyup = function () {
   validateLastName(lastName.value);
-  validateEmail(Email.value);
+};
+email.onkeyup = function () {
+  validateEmail(email.value);
+};
+web.onkeyup = function () {
   validateWeb(web.value);
-  validateMessage(message.value);``
-})
+}
+message.onkeyup = function () {
+  validateMessage(message.value);
+}
+
+submitButton.addEventListener("click", function () {
+  validateFirstName(firstName.value);
+
+  validateLastName(lastName.value);
+
+  validateEmail(email.value);
+
+  validateWeb(web.value);
+
+  validateMessage(message.value);
+});
